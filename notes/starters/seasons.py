c9e7d06887e4a02d3de73fc23797e1afb3629302
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-08-21 09:52:31 -0600

    variables notes

---
+++ notes/starters/seasons.py
@@ -0,0 +1,7 @@
+#IC, 1st seasons 
+ 
+last_name = input("what is your last name: ")
+season = input ("what is your least favorite season: ")
+
+
+print (f"wow, {last_name} san, I don't like {season} too")
---
+++ notes/variables.py
@@ -0,0 +1,7 @@
+# Ic 1st variables Notes
+even_number = 2 
+grade = 99
+animal = "Panda"
+print(grade)
+grade = 95
+print(grade)
