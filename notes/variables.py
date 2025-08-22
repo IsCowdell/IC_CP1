ed12ea0c1d3af281d0284ac25d8cbcffdc244a3e
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-08-22 09:38:34 -0600

    who_are_you_quiz

--- notes/variables.py
+++ notes/variables.py
@@ -4,4 +4,6 @@ grade = 99
 animal = "Panda"
 print(grade)
 grade = 95
-print(grade)
+print(grade) 
+# the variables are num1,num2,num3,num4, and result 1 and result.
+# they don't show the information we need.
---
+++ practices/who_are_you.py
@@ -0,0 +1,7 @@
+#IC - Who are you quiz
+
+name = input("what is your name?: ")
+age = input("how old are you?: ")
+fav_color = input("what is your favorite color?: ")
+
+print("Hello",(name),"I am",(age),"too!"  " You like",(fav_color),"too?")
