07eb514b5a16aef851ddeed2906021f22a2e9b7e
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-08-22 09:40:41 -0600

    Finished_who_are_you_quiz

--- practices/who_are_you.py
+++ practices/who_are_you.py
@@ -4,4 +4,4 @@ name = input("what is your name?: ")
 age = input("how old are you?: ")
 fav_color = input("what is your favorite color?: ")
 
-print("Hello",(name),"I am",(age),"too!"  " You like",(fav_color),"too?")
+print("Hello",(name),"I am",(age),"too!"  " you like",(fav_color),"too?")
