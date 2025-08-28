0fbd045e2bb5868bfad8530b1f28077f23fd43ae
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-08-28 09:53:20 -0600

    Average grade project

---
+++ projects/Average_Grade.py
@@ -0,0 +1,15 @@
+# IC, 1st Average Grade Project
+
+math_grade = input("what is your grade in math ")
+english_grade = input("what is your grade in english")
+art_grade = input("what is your grade in art")
+seminary_grade = input("what is your grade in seminary")
+biology_grade = input("what is your grade in biology")
+coding_grade = input("what is your grade in coding")
+world_civ_grade = input("what is your grade in world civillzations")
+
+int(world_civ_grade + coding_grade + biology_grade + math_grade + seminary_grade + english_grade + art_grade/7)
+
+
+
+
