3d4afce6b3d746371941ebda44fed1112bf1bf90
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-08-29 03:01:44 +0000

    average_grade

--- projects/Average_Grade.py
+++ projects/Average_Grade.py
@@ -8,7 +8,7 @@ biology_grade = input("what is your grade in biology")
 coding_grade = input("what is your grade in coding")
 world_civ_grade = input("what is your grade in world civillzations")
 
-int(world_civ_grade + coding_grade + biology_grade + math_grade + seminary_grade + english_grade + art_grade/7)
+print("Your average grade is: " + str(round((int(math_grade)+int(english_grade)+int(art_grade)+int(seminary_grade)+int(biology_grade)+int(coding_grade)+int(world_civ_grade))/7,2)))
 
 
 
