0a3d2f8f431055a798130eddc7ce663775183434
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-09-03 18:11:34 -0600

    Update madlib.py

--- projects/madlib.py
+++ projects/madlib.py
@@ -7,6 +7,6 @@ action = input("Give me an action  " )
 theme = input("give me a theme")
 food = input("give me a food")
 
-print("It was a great day before" + name+ showed up, It was my best friend's birthday part. (name) completely ruined the (vibe) by (action) on the couch. At least the theme (theme) of the party was amazing and fun. The best part about the party was defintely the (food) it was so tasty. ")
+print("It was a great day before" +name+ "showed up, It was my best friend's birthday part." (name) "completely ruined the" (vibe) "by" (action) "on the couch. At least the theme" (theme) "of the party was amazing and fun. The best part about the party was defintely the" (food) "it was so tasty. ")
  
 
