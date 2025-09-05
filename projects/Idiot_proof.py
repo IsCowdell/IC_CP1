ae2bd7200d4953916655f7d2245a187e44630283
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-09-05 09:53:15 -0600

    Idiot proof

---
+++ projects/Idiot_proof.py
@@ -0,0 +1,19 @@
+#IC - 1st Idiot proof
+
+
+
+first_name = input("what is your first name").strip().title()
+
+last_name = input("What is your last name? ").strip().title()
+
+phone_number = str(float(input("what is your phone number")))
+
+gpa = input("what is your GPA? ").strip()
+
+full_name = first_name + " " + last_name
+print("name:",full_name)
+
+phone_1 = phone_number[0.3]
+phone_2 = phone_number[3.6]
+phone_3 = phone_number[6.10]
+print(phone_1 + phone_2 + phone_3)
