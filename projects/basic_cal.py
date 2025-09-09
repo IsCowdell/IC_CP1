cc586a19d1e939b6ded10065972cfe44a00cf6b2
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-09-09 09:49:31 -0600

    finsihed basic calculator

--- projects/basic_cal.py
+++ projects/basic_cal.py
@@ -3,7 +3,18 @@
 number1 = float(input("what is your favorite number: "))
 number2 = float(input("what is your least favorite number: "))
 
+div = round(number1/number2,2)
+subtraction = round(number1 - number2,2)
+add = round(number1 + number2,2)
+multi = round(number1 * number2,2)
+floor_div = round(number1//number2,2)
+mod = round(number1%number2,2)
+exponets = round(number1**number2,2)
 
-subtraction = number1 - number2
-add = number1 + number2
-print(f"this is your additon equation" "this is your answer")
+print(f"{number1} + {number2} = {add}")
+print(f"{number1} - {number2} = {subtraction}")
+print(f"{number1} * {number2} = {multi}")
+print(f"{number1} / {number2} = {div}")
+print(f"{number1} // {number2} = {floor_div}")
+print(f"{number1} % {number2} = {mod}")
+print((f"{number1} ** {number2} = {exponets}"))
