cf35b4503132321e4380e8b042033f60e6a5e994
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-09-25 09:52:51 -0600

    debugging notes

---
+++ notes/debuggin.py
@@ -0,0 +1,15 @@
+#☑️debugging IC 1st 
+
+
+#SyntaxError - grammar error 
+
+    #print"vienna"
+#fixing error by reading the message 
+
+#Logic Error - gave wrong steps - wrong output
+num1 = "2"
+num2 = "5"
+
+print(num1 + num2)
+
+#Runtime errors (when it runs)  
