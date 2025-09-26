d3495a168aea4c58b91ce8028199fae219eb6129
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-09-26 09:52:48 -0600

    debugging

--- notes/debuggin.py
+++ notes/debuggin.py
@@ -12,4 +12,14 @@ num2 = "5"
 
 print(num1 + num2)
 
-#Runtime errors (when it runs)  
+#Have a well though out plan step by step work through your logic
+
+
+#Runtime errors (when it runs) 
+import random
+while True:
+
+    denominator = random.randint(1,5)
+
+print(18/denominator)
+  
