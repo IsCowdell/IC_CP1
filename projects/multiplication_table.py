ef624150c571efe22ee2f0e48c53ad10ef52b62d
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-10-02 09:52:58 -0600

    multiplication

---
+++ projects/multiplication_table.py
@@ -0,0 +1,18 @@
+#IC multiplication_table
+
+num = [1,2,3,4,5,6,7,8,9,10,11,12]
+numa = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+list_secound = []
+complete_list = []
+
+for i in num:
+    numa = i 
+    for y in numa:
+      complete_list = i*y
+      list_secound.append[complete_list]
+
+print(complete_list)
+
+
+
