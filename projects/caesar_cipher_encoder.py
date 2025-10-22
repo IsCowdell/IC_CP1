698d9a57f2ec34e25983213ad189b06379b0372e
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-10-22 09:52:51 -0600

    caesar

---
+++ projects/caesar_cipher_encoder.py
@@ -0,0 +1,13 @@
+#IC 1st caesar cipher encoder
+
+#ask user what they want to do 
+ask_user = input("Do you want to decode or encode")
+#defining number value
+number = list(range(1,27))
+print(number)
+#make the lowercase and uppercase alphabet 
+lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+uppercase = ["A","B","C","D","E","F","G","H","I","J","k","l","","N","O","p","Q","R","S","T","U","V","W","X","Y","Z"]
+lowercase(index(l,1))
+    
+
