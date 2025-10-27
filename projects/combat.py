bb470640e91cdc1cc5a61fe37152cc97a082bf9b
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-10-27 09:53:09 -0600

    combat

--- projects/combat.py
+++ projects/combat.py
@@ -1,12 +1,14 @@
 #IC 1st combat
 
 import random
-mage = 1
-thief = 2
-dora = 7 
+enemy = "HGUHSIUhgUHSDU"
+enemy_health = 45
+enemy_hit = 23
+talentenemy = 21
+combat = 11
 username = input("What is your username")
 
-character = input(f"If you want to be a mage press 1 if you want to be a thief press 2 and if you want to be dora press 7: ")
+character = input(f"If you want to be a mage press 1 thief press 2 and dora press 7 and rat 5: ")
 
 if character == "1":
     print("your a mage")
@@ -16,7 +18,7 @@ if character == "1":
     talent = 2 
     strength = 5
 else:
-    print()
+   
     if character == "2":
          print("your a thief")
          print("this is your stats strength = 2 combat = 3 and talent = 24 health = 22")
@@ -25,23 +27,35 @@ else:
          talentthief = 24
          health = 22 
     else:
-         print()
+        
+         if character == "7":
+              print("you are dora")
+              print("this is your stats strength = 70 combat = 57 talent = 78 and healt is 67")
+              dorastrength = 70
+              combatdora = 57
+              talent = 78
+              health = 67
+         else:
+          
+              if character == "5":
+                    print("you are a rat")
+                    print('you got a stepped on')
+                    
+              else: 
+                    print()
 
+turn_counter = random.randint(1,20)
 fighting = input("do you want to fight a monster yes or no: ")
 if fighting == "yes":
      print("you are going against HGUHSIUhgUHSDU")
      print("let see if you go first")
-     if random.randint <= 10:
+     if turn_counter >= 10:
             print("you go first")
-            attack = input("what do you want to do 1 or 2")
-            if attack == "1":
-                 print("wow did a magic twirly took off 35 of his health he has 22 left")
-                 print("monster turn")
-            else:
-                 print("you did a flying mokey")
-                 print("YOU KILLED THE MONSTER!")
      else:
             print("monster goes first")
-
+                 
+            
         
+      
+    
     
