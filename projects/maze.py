1a45da572a7ce800cf17a353cc473181ca7cfc57
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-11-05 09:53:08 -0700

    maze

--- projects/maze.py
+++ projects/maze.py
@@ -8,31 +8,29 @@ t.setup(1000,1000)
 
 row_grid = [[1,1,0,0,1,0],[0,1,1,1,0,1]]
 col_grid = [[0,1,1,0,0,1],[0,0,0,1,1,1]]
+mick = t.Turtle()
+joe = mick.clone()
+sharah = mick.clone()
+mick.color("green")
+
+mick.penup()
+mick.forward(50)
+mick.pendown()
+mick.forward(250)
+mick.right(90)
+mick.forward(-300)
+joe.left(90)
+joe.forward(300)
+joe.right(90)
+joe.forward(250)
+joe.penup()
+joe.forward(50)
+joe.pendown()
 
 
-for x in range(4):
-    t.right(90)
-    t.forward(300)
 
-t.right(90)
-t.forward(200)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
 
-t.penup()
-t.right(90)
-t.forward(300)
-t.right(90)
-t.pendown()
-t.forward(100)
-t.right(-36)
-t.forward(100)
+   
 
 def is_sovlable(row_grid, col_grid):
     size = len(row_grid) - 1
