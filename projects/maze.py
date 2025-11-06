894cd92513073e303139ee8271c0ea04a8c5ace3
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-11-06 09:49:44 -0700

    MAZERR

--- projects/maze.py
+++ projects/maze.py
@@ -3,11 +3,17 @@
 yes = True
 import turtle as t
 import random 
+
 t.Screen()
 t.setup(1000,1000)
-
-row_grid = [[1,1,0,0,1,0],[0,1,1,1,0,1]]
-col_grid = [[0,1,1,0,0,1],[0,0,0,1,1,1]]
+t.speed(900)
+random_options = random.randint(0,1)
+row_grid = [[1,1,0,0,1,0],
+            [0,1,1,1,0,1]]
+col_grid = [[0,1,1,0,0,1],
+            [0,0,0,1,1,1]]
+target_value = 1
+target_value2 = 0
 mick = t.Turtle()
 joe = mick.clone()
 sharah = mick.clone()
@@ -30,7 +36,24 @@ joe.pendown()
 
 
 
-   
+def rowmaze(row_grid,random_options,target_value):
+    if target_value in row_grid:
+        for i in range(len(row_grid)):
+            if row_grid[i] == target_value:
+                row_grid[i] = random_options(0,1)
+    return rowmaze
+
+
+def colmaze(col_grid,random_options,target_value2):
+    if target_value in col_grid:
+        for c in range(len(col_grid)):
+            if col_grid[c] == target_value2: 
+                col_grid[c] == random_options(0,1)
+
+
+    
+        
+    
 
 def is_sovlable(row_grid, col_grid):
     size = len(row_grid) - 1
