0dd85ea9475a2995101fa9976162ca75ddc10314
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-11-12 09:53:07 -0700

    order

--- projects/order_up.py
+++ projects/order_up.py
@@ -1,25 +1,25 @@
 #IC 1st order up
-
+yes = True 
 drinks = {
-    "coke" : 1,
-    "water" :13,
-    "sprite" : 3.99,
-    "apple juice" : 70
+    'coke' : 1,
+    'water' : 13,
+    'sprite' : 3.99,
+    'apple juice' : 70
           }
 
 
 side_dishes = {
-    "fries" : 10,
-    "salad" : 23,
-    "soup" : 5,
+    'fries' : 10,
+    'salad' : 23,
+    'soup' : 5,
     "rice" : 2
 }
 
 main_dishes = {
-    "hamburger" : 5,
-    "pasta" : 12,
-    "steak" : 1,
-    "slop" : 300
+    'hamburger' : 5,
+    'pasta' : 12,
+    'steak' : 1,
+    'slop' : 300
 
 
 }
@@ -29,14 +29,17 @@ order = []
 print("this is our menu ")
 print(side_dishes,main_dishes,drinks)
 
+
+
 drink_choice = input("choose what you want for your drink: ").lower().strip()
 if drink_choice in drinks: 
     print("We have that drink")
     order.append(drink_choice)
     print(f"this is order so far {order}")
 else:
-    print("we don't have that drink try again")
-    quit
+    print("we dont have that drink")
+
+
 
 
 main_choice = input("choose what you want for your main dish: ").lower()
@@ -47,11 +50,29 @@ if main_choice in main_dishes:
     print(f"this is order so far {order}")
 else:
     print("we don't have that dish try again")
-    quit
 
-first_sidedish = input("choose first side dishes: ").lower
+sidedish1 = input("choose first side dishes: ").lower()
 
-sec_sidedish = input("choose second side dishes: ").lower
+if sidedish1 in side_dishes:
+    print("we have that dish")
+    order.append(sidedish1)
+    print(f"this is order so far {order}")
+else:
+    print("we don't have that dish try again")
 
 
 
+sidedish = input("choose second side dishes: ").lower()
+
+if sidedish in side_dishes:
+    print("we have that dish")
+    order.append(sidedish)
+    print(f"this is order so far {order}")
+else:
+    print("we don't have that dish try again")
+
+
+final_order = order
+
+
+print(f"this is your final order{order} and ")
