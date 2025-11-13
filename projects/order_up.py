e17851a8cfd8ffdb7996d4f47e260fc2a4d4cf78
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-11-13 09:53:12 -0700

    order up

--- projects/order_up.py
+++ projects/order_up.py
@@ -1,40 +1,54 @@
 #IC 1st order up
-yes = True 
+#importing prints
+import pprint
+
+
+
+
+#creating a dctornary for drinks
 drinks = {
     'coke' : 1,
     'water' : 13,
     'sprite' : 3.99,
     'apple juice' : 70
           }
+#making it print very pretty 
 
+pprint.pprint(f"this is our drinks {drinks}")
 
+#defing the side dishes with prices 
 side_dishes = {
     'fries' : 10,
     'salad' : 23,
     'soup' : 5,
     "rice" : 2
 }
-
+#time to print them very pretty
+pprint.pprint(f"this is our side dishes{side_dishes}")
+#defing main dishes and price
 main_dishes = {
     'hamburger' : 5,
     'pasta' : 12,
     'steak' : 1,
     'slop' : 300
-
-
 }
-
+#more pretty printing 
+pprint.pprint(f"this is our main dishes{main_dishes}")
+#creating order so they can fill it up
 order = []
 
-print("this is our menu ")
-print(side_dishes,main_dishes,drinks)
 
 
 
+
+#letting user choose what they want for their drink
 drink_choice = input("choose what you want for your drink: ").lower().strip()
 if drink_choice in drinks: 
     print("We have that drink")
+    drinkprice =  drinks[drink_choice]
     order.append(drink_choice)
+    print(f"this is the price of your drink ${drinkprice}")
+
     print(f"this is order so far {order}")
 else:
     print("we dont have that drink")
@@ -42,19 +56,24 @@ else:
 
 
 
-main_choice = input("choose what you want for your main dish: ").lower()
+main_choice = input("choose what you want for your main dish: ").lower().strip()
 
 if main_choice in main_dishes:
     print("we have that dish")
+    mainprice =  main_dishes[main_choice]
+    print(f"This is the price of your main dish ${mainprice}")
+
     order.append(main_choice)
     print(f"this is order so far {order}")
 else:
     print("we don't have that dish try again")
 
-sidedish1 = input("choose first side dishes: ").lower()
+sidedish1 = input("choose first side dishes: ").lower().strip()
 
 if sidedish1 in side_dishes:
     print("we have that dish")
+    side1price =  side_dishes[sidedish1]
+    print(f"This is the price of your side dish ${side1price}")
     order.append(sidedish1)
     print(f"this is order so far {order}")
 else:
@@ -66,13 +85,15 @@ sidedish = input("choose second side dishes: ").lower()
 
 if sidedish in side_dishes:
     print("we have that dish")
+    side2price =  side_dishes[sidedish]
+    print(f"This is the price of your side dish ${side2price}")
     order.append(sidedish)
     print(f"this is order so far {order}")
 else:
     print("we don't have that dish try again")
 
 
-final_order = order
+finalcost = (side1price + side2price + drinkprice + mainprice)
 
 
-print(f"this is your final order{order} and ")
+print(f"this is your final order{order} and the final cost ${finalcost} ")
