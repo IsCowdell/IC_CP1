7c611b874721d32fe3644d1902f9825c2b917be9
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-11-19 09:26:58 -0700

    notes

---
+++ notes/mapping.py
@@ -0,0 +1,10 @@
+#IC 1st
+
+numbers = [678,657,654,646,456,456]
+
+def divide(num):
+    return num/3
+new_nums =map(lambda num :num/3,numbers)
+print(new_nums)
+for nums in new_nums:
+    print(nums)
