1560dfe5d76a65cebc82b0b605f683d648706943
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-11-25 09:52:24 -0700

    final project psecode

---
+++ projects/Final_project.py
@@ -0,0 +1,31 @@
+#IC 1st Dinosaur train final project 
+
+#Make a function for mental state 
+# mental = 100
+#Function for health 
+#health = 100
+#strength = 50 
+# charcters 
+# dictionary for inverntory
+#buddy 
+# this will be your guide and explain each room to you 
+# print hi my name is buddy I will guide you
+# def function family car 
+#right now you are in the family car
+# the room is empty but a bed the bed does nothing 
+#user choice 
+#input("do you want to leave the room or touch the bed")
+#if user_choice == "leave room"
+# then input("what room do you want to go to dinning room or the play area")
+# def function sercet tunnels 
+#elif print("you are in the secret tunnel")
+#print(" you found Henry Hermit Crab do you want to pet him or countine foward")
+# if pet him then 
+# print(" you got a roasted crab it can increase your heath by 10 points ")
+# print(" when the option is given to you open your invertory by pressing 1")
+# add roasted crab to invetory
+# subtract mental state by 20 
+# print("your mental state is now 80 make sure it doesn't get to low like 20")
+#else
+#incorrect vaule send back up 
+#if user_choice == "play area"
