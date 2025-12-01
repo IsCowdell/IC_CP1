3907f88cfef344e89eb9f4ce838c5a7bb9f3937a
IsCowdell <isabella.cowdell@ucas-edu.net>
2025-12-01 09:48:53 -0700

    Final project

--- projects/Final_project.py
+++ projects/Final_project.py
@@ -17,7 +17,7 @@
 #input("do you want to leave the room or touch the bed")
 #if user_choice == "leave room"
 # then input("what room do you want to go to dinning room or the play area")
-# def function sercet tunnels 
+# def function secret  tunnels 
 #elif print("you are in the secret tunnel")
 #print(" you found Henry Hermit Crab do you want to pet him or countine foward")
 # if pet him then 
@@ -25,7 +25,57 @@
 # print(" when the option is given to you open your invertory by pressing 1")
 # add roasted crab to invetory
 # subtract mental state by 20 
-# print("your mental state is now 80 make sure it doesn't get to low like 20")
+# print(f"your mental state is now {mental state} make sure it doesn't get to low like 20")
+# if "countine foward"
+#show the user you got out of the tunnel you are in the play area
 #else
 #incorrect vaule send back up 
 #if user_choice == "play area"
+#  buddy "You enter a darkish yellow room with a smell of apple sauce and tears"
+# there is a ball pit looking deeper than your sorrows(ik you have a lot)
+# and a slide looking higher than your dreams
+# or you could leave
+# user choice(" where to ball pit or slide or leave ") 
+
+# if ball pit 
+# print(you jump into the pit you can just smell the amount of socks in it )
+#(you can jump out)
+# if "jump out"
+# you get out and (leave function)
+# else:
+# you swim around and found a dollar 
+# (keep it? y\n)
+# if yes add to inventory() 
+# else 
+# leave function()
+#
+# 
+# leave function 
+# and go to *crash*
+# Don gets in your way asking to play a game. 
+# don stats
+#strength = 24
+#health = 80 
+# You could defiently take this but its just a kid 
+# do you want to play a game with him, y\n
+# if ans = "yes"
+# you are now playing rock or scissors no paper 
+# 1 = rock 
+# 2 = scissors 
+# show the user " chose one 1 or 2"
+# if choose = 1 
+# print(" You won")
+# Don gets mad and cries 
+# the cries get unbearable
+# (" you wonder what you can do to make them stop")
+#(you look down at your hands wondering if they could.... its just a kid.)
+#(the cries just stop you look down his neck in your hands.)
+#mental - 20 
+#(f"Your mental state is {mental} low get it up) 
+#
+# else
+# print("Don won but he doesn't look happy.....")
+# 
+
+
+#
